b866f53214ff4fb858f04ac60cc60cb2f80cadd6
Hugo Tunius <h@tunius.se>
2019-12-01 20:21:09 +0000

    [day01] Solve day01

--- src/day01.rs
+++ src/day01.rs
@@ -1,9 +1,31 @@
+fn fuel(mass: i64) -> i64 {
+    (((mass as f64) / 3.0).floor() - 2.0) as i64
+}
+
+fn recursive_fuel(mass: i64, acc: i64) -> i64 {
+    let new_mass = fuel(mass);
+
+    if new_mass > 0 {
+        recursive_fuel(new_mass, acc + new_mass)
+    } else {
+        acc
+    }
+}
+
+fn parse(input: &str) -> impl Iterator<Item = i64> + '_ {
+    input
+        .lines()
+        .map(|line| line.trim())
+        .filter(|l| l.len() > 0)
+        .map(|value| value.parse().expect("Expected only numbers"))
+}
+
 pub fn star_one(input: &str) -> i64 {
-    0
+    parse(input).map(fuel).sum()
 }
 
 pub fn star_two(input: &str) -> i64 {
-    0
+    parse(input).map(|m| recursive_fuel(m, 0)).sum()
 }
 
 #[cfg(test)]
@@ -12,11 +34,11 @@ mod tests {
 
     #[test]
     fn test_star_one() {
-        assert_eq!(star_one(""), 1)
+        assert_eq!(star_one("12\n12"), 4);
     }
 
     #[test]
     fn test_star_two() {
-        assert_eq!(star_two(""), 1)
+        assert_eq!(star_two("14\n1969\n100756"), 2 + 966 + 50346);
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -43,8 +43,8 @@ mod tests {
 
         let input = load_file("day01.txt");
 
-        assert_eq!(star_one(&input), 1);
-        assert_eq!(star_two(&input), 1);
+        assert_eq!(star_one(&input), 3506577);
+        assert_eq!(star_two(&input), 5256960);
     }
 
     #[test]
