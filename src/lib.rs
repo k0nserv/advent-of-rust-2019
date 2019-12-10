28e9831e19dd876cbd395fe6950a1de984c5523e
Hugo Tunius <h@tunius.se>
2019-12-10 16:54:31 +0000

    [lib] Time day 09

--- src/lib.rs
+++ src/lib.rs
@@ -292,8 +292,10 @@ mod tests {
 
         let input = load_file("day09.txt");
 
-        assert_eq!(star_one(&input), 2453265701);
-        assert_eq!(star_two(&input), 80805);
+        time("Day 09, Part 1", || {
+            assert_eq!(star_one(&input), 2453265701)
+        });
+        time("Day 09, Part 2", || assert_eq!(star_two(&input), 80805));
     }
 
     #[test]
